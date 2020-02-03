381e1d50d005058b78dea44c859fd12982ef7fe0
jordan <abc@example.com>
2020-02-03 14:02:07 -0500

    Allow code to call different functions based on command line arguments' '

--- 306PRE_project.c
+++ 306PRE_project.c
@@ -1,10 +1,53 @@
 # include <stdio.h>
 
 int main(int argc, char*argv[]){
-		///TODO: READ COMMAND ARGUEMENTS AND CALL BELOW FUNCTIONS 
+		//READS COMMAND ARGUEMENTS AND CALL BELOW FUNCTIONS 
+		
+		if(argc < 2){ 		//IF NOT ENOUGH ARGUEMENTS PROVIDED
+			printf("No argument provided");
+			return 1;
+		}
+		
+		for(int x = 1; x < argc; x++){
+			if (strncmp("-f", argv[x], 2) == 0){
+				f();
+			}
+			else if (strncmp("-r", argv[x], 2) == 0){
+				r();
+			}
+		
+			else if (strncmp("-h", argv[x], 2) == 0){
+				h();
+			}
+		
+			else if (strncmp("-max", argv[x], 4) == 0){
+				x++;
+				max(argv[x]);
+			}
+		
+			else if (strncmp("-min", argv[x], 4) == 0){
+				x++;
+				min(argv[x]);
+			}
+		
+			else if (strncmp("-mean", argv[x], 5) == 0){
+				x++;
+				mean(argv[x]);
+			}
+		
+			else if (strncmp("-records", argv[x], 8) == 0){
+				x++;
+				records(argv[x]);
+			}
+			else if (x + 1 == argc){	//LAST ARGUEMENT SHOULD BE CSV FILE SO CALL READCSV
+				readcvs(argv[x]);
+				return 0;
+			}
+		}
+		
 }
 
-void readcvs(){
+void readcvs(char field[]){
 		///TODO: READ AND PARSE CSV FILE
 }
 
@@ -20,18 +63,18 @@ void h(){
 		///TODO: Treat the first record of file as a header record rather than a data record
 }
 
-void max(){
+void max(char field[]){
 		///TODO: Display the maximum value in the indicated field of the data records
 }
 
-void min(){
+void min(char field[]){
 		///TODO: Display the minimum value in the indicated field of the data records
 }
 
-void mean(){
+void mean(char field[]){
 		///TODO: Display the arithmetic mean (average) value in the indicated field of the data records
 }
 
-void records(){
+void records(char field[]){
 		///TODO: Display the records from file containing value in the the indicated field
 }
